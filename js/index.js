840037e6187b70adff67a655e0cced6efedaf94e
Bondhan Mondal <bondhan2725@gmail.com>
2022-03-08 00:51:15 +0600

    task 2 complete

--- js/index.js
+++ js/index.js
@@ -1,2 +1,11 @@
 const topPlayer = document.getElementById('top-player');
 topPlayer.style.color = 'blue';
+
+const topBlog = document.getElementById('top-blog');
+topBlog.style.color = 'blue';
+
+const playerSection = document.getElementById('player-section');
+playerSection.style.backgroundColor = 'rgba(163, 149, 132, 0.4)';
+playerSection.style.borderRadius = '10px';
+
+// add item button event handler
