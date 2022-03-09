c3af239f5a35541469629ea49b160787ee483333
Bondhan Mondal <bondhan2725@gmail.com>
2022-03-10 00:33:30 +0600

    calculate total added using function

--- js/shopping-cart.js
+++ js/shopping-cart.js
@@ -12,6 +12,25 @@ function updateProductNumber(product, price, isIncreasing){
     //update total
     const productTotal = document.getElementById(product + '-total');
     productTotal.innerText = parseInt(productNumber) * price;
+
+    // calling function to calculate total
+    calculateTotal();
+}
+
+function getProductNumber(product){
+    const productInput = document.getElementById(product + '-number');
+    const productNumber = parseInt(productInput.value);
+    return productNumber;
+}
+function calculateTotal(){
+    const phoneTotal = getProductNumber('phone') * 1219;
+    const caseTotal = getProductNumber('case') * 59;
+    const subTotal = phoneTotal + caseTotal;
+    const tax = (subTotal * 10) / 100;
+    const total = subTotal + tax;
+    document.getElementById('sub-total').innerText = subTotal;
+    document.getElementById('tax-amount').innerText = tax;
+    document.getElementById('total-price').innerText = total;
 }
 
 
@@ -22,20 +41,20 @@ function updateProductNumber(product, price, isIncreasing){
 document.getElementById('phone-plus').addEventListener('click', function(){
     updateProductNumber('phone', 1219, true)
 
-})
+});
 
 //phone decrese
 document.getElementById('phone-minus').addEventListener('click', function(){
     updateProductNumber('phone', 1219, false)
 
-})
+});
 
 //case increase
 document.getElementById('case-plus').addEventListener('click', function(){
     updateProductNumber('case', 59, true);
-})
+});
 
 //case decrease
 document.getElementById('case-minus').addEventListener('click', function(){
     updateProductNumber('case', 59, false);
-})
+});
