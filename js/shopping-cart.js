45bb97b396b8f6cb636fa70cc36f5943307aa5b1
Bondhan Mondal <bondhan2725@gmail.com>
2022-03-09 23:56:39 +0600

    phone number increase decrease with phone total update without function

--- js/shopping-cart.js
+++ js/shopping-cart.js
@@ -1,1 +1,32 @@
-console.log('connected');
+
+
+
+
+
+
+//phone incerase 
+document.getElementById('phone-plus').addEventListener('click', function(){
+    const phoneNumberInput = document.getElementById('phone-number');
+    let phoneNumberValue = parseInt(phoneNumberInput.value);
+    phoneNumberValue = phoneNumberValue + 1;
+    phoneNumberInput.value = phoneNumberValue;
+//updating phone total
+    const phoneTotalField = document.getElementById('phone-total');
+    //const phoneTotalValue = parseInt(phoneTotalField.innerText);
+    //const phoneTotal = phoneNumberValue * 1219;
+    phoneTotalField.innerText = phoneNumberValue * 1219;
+
+})
+
+//phone decrese
+document.getElementById('phone-minus').addEventListener('click', function(){
+    const phoneNumberInput = document.getElementById('phone-number');
+    let phoneNumberValue = parseInt(phoneNumberInput.value);
+    if(phoneNumberValue > 0){
+        phoneNumberValue = phoneNumberValue - 1;
+        phoneNumberInput.value = phoneNumberValue;
+    }
+    const phoneTotalField = document.getElementById('phone-total');
+    phoneTotalField.innerText = phoneNumberValue * 1219;
+
+})
